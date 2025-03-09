8f0b745404c38fcbf4b83bf9cabc3a321ee6f9ec
Felipe Fernandes <85773727+rkhue@users.noreply.github.com>
2025-03-09 00:20:20 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -167,3 +167,287 @@ db.produto.insertMany([
     "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
   }
 ]);
+
+db.produto.insertMany([
+  // Jugemu (Barato, bastante desconto)
+  {
+    "categoria": ["Salgado"],
+    "desconto": 3.0,
+    "descricao": "Onigiri simples com recheio de atum",
+    "marca": "Jugemu",
+    "nome": "TunaBall",
+    "preco": 12.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 2.0,
+    "descricao": "Dango doce com calda de açúcar",
+    "marca": "Jugemu",
+    "nome": "SugarDango",
+    "preco": 14.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiW98sgaelSwCCHQShK-HmW-2Mu6KyFSxunIX68ZR-gUhh7oTBrBXH8y3byHIe9ixrsA3cgLsH06o3UI83TD4WFOx2sair0XnT660TMcePdrn2d4Ut9BIyt0APHroWNyxvaRBT0WJQWJATR/s800/sweets_dango06_mitarashi.png"
+  },
+  {
+    "categoria": ["Bebida"],
+    "desconto": 1.5,
+    "descricao": "Chá verde gelado simples",
+    "marca": "Jugemu",
+    "nome": "GreenIced",
+    "preco": 6.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGuOeE4kXo24Zu9VIylDTebQsDt_v1cKybA9kfYBOBpJ_EOrOHMwkKMYK2BHLyunujsMPX-dAn3VALILAOyL8-hhdm-_HCHZRSym_JX93xsJg-dcnqmeevGD-FF94CJ7tQ6pfiRKYO2Vc/s800/tea_lemon.png"
+  },
+
+  // Yamaha (Barato)
+  {
+    "categoria": ["Salgado", "Quente"],
+    "desconto": 0,
+    "descricao": "Onigiri grelhado com shoyu",
+    "marca": "Yamaha",
+    "nome": "GrillSoy",
+    "preco": 15.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 1.0,
+    "descricao": "Donut com cobertura de chocolate",
+    "marca": "Yamaha",
+    "nome": "ChocoRing",
+    "preco": 13.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgLIufLMGhZntmXSNGTirbVttwnqAzCQxvIpuOfZMaGOR_ZOunlOPP1kTfxnoB0g1-97G_UgVkxR2dGCOufxsragaDveSews1MWzfao2r-kOJjHkU1rAfB-6nEWlhM5Aacsc-67kOk7rg0B/s800/sweets_donut.png"
+  },
+  {
+    "categoria": ["Bebida", "Gelado"],
+    "desconto": 0.5,
+    "descricao": "Soda refrescante de limão",
+    "marca": "Yamaha",
+    "nome": "LemonFizz",
+    "preco": 7.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgjhwvBzyP7KBRHD8T-XzMnAmn4Jm0x23e40S-LrxsSglGjlK_HIgZEOPQJU8Mmpny7qqU9Wr6pHw2E6_in6Kz49rN9P693dlJT5WIW0X8X1A7xAsPgiXlTEFvQYEjYEl6fYnEHyE9bzy5Z/s1600/drink_cola_petbottle.png"
+  },
+
+  // Sakura (Mediana, preços na média)
+  {
+    "categoria": ["Salgado"],
+    "desconto": 1.4,
+    "descricao": "Onigiri branco clássico com toque de gergelim",
+    "marca": "Sakura",
+    "nome": "Shirogiri",
+    "preco": 19.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Salgado", "Quente"],
+    "desconto": 2.5,
+    "descricao": "Onigiri grelhado com crosta crocante",
+    "marca": "Sakura",
+    "nome": "YakiGiri",
+    "preco": 27.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida"],
+    "desconto": 1.0,
+    "descricao": "Café quente suave",
+    "marca": "Sakura",
+    "nome": "SmoothBrew",
+    "preco": 9.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgm7j4O9Fre9FhRX8tdlNhppCwhjAkoOPtG4s0-j8ClpkGFCix3cpZ9Kys7usPGEpOBmGGTZ70wgZSJEXKog8MP_BV0JJIHYEV9zPfjdJnOiWV2eWiCIZcMMP5-Wfyiv5r9caNxvFAm0Mg/s400/cafe_coffee_cup.png"
+  },
+
+  // Takaru (Cara, porém acessível)
+  {
+    "categoria": ["Doce"],
+    "desconto": 1.5,
+    "descricao": "Dango tricolor premium",
+    "marca": "Takaru",
+    "nome": "TriadDango",
+    "preco": 25.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4nhiR-t6nn8j42_I5WNuNwMB41bEL9S32AjQfKp52u1WOfQTeKjL4w5dXxXlc1a19mVi-DKO8V3uB1POkKAM1J5ejToKYg8rtMtWhfMzaQTPuMhG5TKTIXvQIjrd8VKn-6MhilzIVS0g/s800/sweets_sansyoku_dango.png"
+  },
+  {
+    "categoria": ["Salgado"],
+    "desconto": 2.0,
+    "descricao": "Onigiri com recheio de camarão",
+    "marca": "Takaru",
+    "nome": "ShrimpBall",
+    "preco": 28.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida", "Gelado"],
+    "desconto": 1.0,
+    "descricao": "Soda artesanal de cereja",
+    "marca": "Takaru",
+    "nome": "CherryPop",
+    "preco": 12.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgjhwvBzyP7KBRHD8T-XzMnAmn4Jm0x23e40S-LrxsSglGjlK_HIgZEOPQJU8Mmpny7qqU9Wr6pHw2E6_in6Kz49rN9P693dlJT5WIW0X8X1A7xAsPgiXlTEFvQYEjYEl6fYnEHyE9bzy5Z/s1600/drink_cola_petbottle.png"
+  },
+
+  // Fujiwara (Careira, melhor qualidade)
+  {
+    "categoria": ["Salgado"],
+    "desconto": 0,
+    "descricao": "Onigiri de salmão com um toque cítrico",
+    "marca": "Fujiwara",
+    "nome": "NigiriSun",
+    "preco": 29.30,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 1.0,
+    "descricao": "Donut artesanal com recheio de creme",
+    "marca": "Fujiwara",
+    "nome": "CreamDream",
+    "preco": 29.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgciOe8t5Tlot1ADpSSXG-SrbtlpglIi3f10aSpOEWKnfaTQFjT5yMhzsVkdT-2cb6boofA7fDZ5Xyf8Cxdvo0YxrvF5oPHfuTAntAhgbChKFZ0UXiG6Z-0kCrWdJ0BbNrN0tNTnwHMjbs7/s800/pan_anko_donut.png"
+  },
+  {
+    "categoria": ["Bebida", "Quente"],
+    "desconto": 0,
+    "descricao": "Café premium com notas de caramelo",
+    "marca": "Fujiwara",
+    "nome": "CaramelBrew",
+    "preco": 14.99,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgm7j4O9Fre9FhRX8tdlNhppCwhjAkoOPtG4s0-j8ClpkGFCix3cpZ9Kys7usPGEpOBmGGTZ70wgZSJEXKog8MP_BV0JJIHYEV9zPfjdJnOiWV2eWiCIZcMMP5-Wfyiv5r9caNxvFAm0Mg/s400/cafe_coffee_cup.png"
+  },
+
+  // Adicionando mais produtos para chegar a 30
+  {
+    "categoria": ["Doce"],
+    "desconto": 0.5,
+    "descricao": "Dango com pasta de feijão doce",
+    "marca": "Jugemu",
+    "nome": "BeanDango",
+    "preco": 13.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKi1wsdV_Y-W-2C-0QwKXyJSGrl2ur1hLmKTA7wvKv_AOCRKaKL6WS26ttuFoS3NafrsoXv26l6Eu7XErRB9LO24wFuDNSQ6NTxJBtdqGcUcH-YRcdJ-blGkYgm9BhszZECuJzJB-0xx8K/s800/sweets_dango02_koshian.png"
+  },
+  {
+    "categoria": ["Salgado"],
+    "desconto": 0,
+    "descricao": "Onigiri com umeboshi",
+    "marca": "Yamaha",
+    "nome": "UmeBall",
+    "preco": 14.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida", "Gelado"],
+    "desconto": 0.8,
+    "descricao": "Chá gelado com limão",
+    "marca": "Sakura",
+    "nome": "LemonTea",
+    "preco": 10.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGuOeE4kXo24Zu9VIylDTebQsDt_v1cKybA9kfYBOBpJ_EOrOHMwkKMYK2BHLyunujsMPX-dAn3VALILAOyL8-hhdm-_HCHZRSym_JX93xsJg-dcnqmeevGD-FF94CJ7tQ6pfiRKYO2Vc/s800/tea_lemon.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 1.2,
+    "descricao": "Donut colorido de unicórnio",
+    "marca": "Takaru",
+    "nome": "UnicornRing",
+    "preco": 26.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7uMiml3LkioMZ0KhAyv0JVzRcNeYqOSsDE_kFV78pmovpnxWQGSizHoTH6DbjuClEJ6mts2dKihrftLPKhj9aqIfrNEB0m_RfVquncIXJuFX8WQPuIgHA2Motg1_rA0ps73fpq8_kDBBU/s800/unicorn_food_sweets_donut.png"
+  },
+  {
+    "categoria": ["Salgado", "Quente"],
+    "desconto": 0,
+    "descricao": "Onigiri grelhado com miso",
+    "marca": "Fujiwara",
+    "nome": "MisoGrill",
+    "preco": 29.80,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida"],
+    "desconto": 2.0,
+    "descricao": "Soda de laranja artesanal",
+    "marca": "Jugemu",
+    "nome": "OrangePop",
+    "preco": 8.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgjhwvBzyP7KBRHD8T-XzMnAmn4Jm0x23e40S-LrxsSglGjlK_HIgZEOPQJU8Mmpny7qqU9Wr6pHw2E6_in6Kz49rN9P693dlJT5WIW0X8X1A7xAsPgiXlTEFvQYEjYEl6fYnEHyE9bzy5Z/s1600/drink_cola_petbottle.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 0,
+    "descricao": "Dango com calda de mitarashi",
+    "marca": "Yamaha",
+    "nome": "MitaDango",
+    "preco": 16.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiW98sgaelSwCCHQShK-HmW-2Mu6KyFSxunIX68ZR-gUhh7oTBrBXH8y3byHIe9ixrsA3cgLsH06o3UI83TD4WFOx2sair0XnT660TMcePdrn2d4Ut9BIyt0APHroWNyxvaRBT0WJQWJATR/s800/sweets_dango06_mitarashi.png"
+  },
+  {
+    "categoria": ["Salgado"],
+    "desconto": 1.0,
+    "descricao": "Onigiri com alga crocante",
+    "marca": "Sakura",
+    "nome": "NoriCrisp",
+    "preco": 22.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida", "Quente"],
+    "desconto": 0.5,
+    "descricao": "Café espresso intenso",
+    "marca": "Takaru",
+    "nome": "EspressoShot",
+    "preco": 13.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgm7j4O9Fre9FhRX8tdlNhppCwhjAkoOPtG4s0-j8ClpkGFCix3cpZ9Kys7usPGEpOBmGGTZ70wgZSJEXKog8MP_BV0JJIHYEV9zPfjdJnOiWV2eWiCIZcMMP5-Wfyiv5r9caNxvFAm0Mg/s400/cafe_coffee_cup.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 0,
+    "descricao": "Dango tricolor artesanal",
+    "marca": "Fujiwara",
+    "nome": "EliteDango",
+    "preco": 29.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4nhiR-t6nn8j42_I5WNuNwMB41bEL9S32AjQfKp52u1WOfQTeKjL4w5dXxXlc1a19mVi-DKO8V3uB1POkKAM1J5ejToKYg8rtMtWhfMzaQTPuMhG5TKTIXvQIjrd8VKn-6MhilzIVS0g/s800/sweets_sansyoku_dango.png"
+  },
+  {
+    "categoria": ["Salgado"],
+    "desconto": 1.5,
+    "descricao": "Onigiri com frango teriyaki",
+    "marca": "Jugemu",
+    "nome": "TeriBall",
+    "preco": 15.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida", "Gelado"],
+    "desconto": 0,
+    "descricao": "Soda de maçã verde",
+    "marca": "Yamaha",
+    "nome": "AppleFizz",
+    "preco": 8.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgjhwvBzyP7KBRHD8T-XzMnAmn4Jm0x23e40S-LrxsSglGjlK_HIgZEOPQJU8Mmpny7qqU9Wr6pHw2E6_in6Kz49rN9P693dlJT5WIW0X8X1A7xAsPgiXlTEFvQYEjYEl6fYnEHyE9bzy5Z/s1600/drink_cola_petbottle.png"
+  },
+  {
+    "categoria": ["Doce"],
+    "desconto": 1.0,
+    "descricao": "Donut com recheio de azuki",
+    "marca": "Sakura",
+    "nome": "AzukiRing",
+    "preco": 23.50,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgciOe8t5Tlot1ADpSSXG-SrbtlpglIi3f10aSpOEWKnfaTQFjT5yMhzsVkdT-2cb6boofA7fDZ5Xyf8Cxdvo0YxrvF5oPHfuTAntAhgbChKFZ0UXiG6Z-0kCrWdJ0BbNrN0tNTnwHMjbs7/s800/pan_anko_donut.png"
+  },
+  {
+    "categoria": ["Salgado", "Quente"],
+    "desconto": 0,
+    "descricao": "Onigiri grelhado com katsu",
+    "marca": "Takaru",
+    "nome": "KatsuGrill",
+    "preco": 27.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5VsD60JSuXklQr5dxCgcxAlKLTgz-FGPBNgiXP0E7NSAZEnPp1WYAZRFbk3jbXFmLyF7snR62YHhf-S5G9zrh22cgCmZcsZq_0H8Ie6Mb_cGFzdfsmlKy85jMam0FTrwsvijBiSemBbID/s180-c/onigiri_maru.png"
+  },
+  {
+    "categoria": ["Bebida", "Quente"],
+    "desconto": 0,
+    "descricao": "Chá preto com especiarias",
+    "marca": "Fujiwara",
+    "nome": "SpiceTea",
+    "preco": 14.00,
+    "url_imagem": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGuOeE4kXo24Zu9VIylDTebQsDt_v1cKybA9kfYBOBpJ_EOrOHMwkKMYK2BHLyunujsMPX-dAn3VALILAOyL8-hhdm-_HCHZRSym_JX93xsJg-dcnqmeevGD-FF94CJ7tQ6pfiRKYO2Vc/s800/tea_lemon.png"
+  }
+]);
